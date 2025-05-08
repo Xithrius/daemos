34355b6f04a938194bb51a3424551fe2edaf4303
Xithrius <xithrius@fastmail.com>
2025-05-07 21:06:33 -0700

    Removed greet component

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,10 +5,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::greet::greet,
-            commands::files::read_music_files
-        ])
+        .invoke_handler(tauri::generate_handler![commands::files::read_music_files])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import Greet from "./components/Greet.vue";
 import Files from "./components/Files.vue";
 </script>
 
 <template>
   <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <Greet />
     <Files/>
   </main>
 </template>
