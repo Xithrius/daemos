e23601364c77e0caf3ad9f24fad5c8959c54525f
Xithrius <xithrius@fastmail.com>
2025-06-27 04:29:11 -0700

    Added back DAEMOS_LOG environment variable and set default log level to info

--- src/logging.rs
+++ src/logging.rs
@@ -4,15 +4,10 @@ use tracing_subscriber::EnvFilter;
 
 pub fn initialize_logging() -> Result<()> {
     let env_filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::DEBUG.into())
-        // .with_env_var("DAEMOS_LOG")
+        .with_default_directive(LevelFilter::INFO.into())
+        .with_env_var("DAEMOS_LOG")
         .from_env_lossy()
         .add_directive("winit=off".parse()?);
-
-    // let env_filter = std::env::var("DAEMOS_LOG")
-    //     .ok()
-    //     .and_then(|var| EnvFilter::try_new(var).ok())
-    //     .unwrap_or_else(|| EnvFilter::new("info"));
 
     let subscriber = tracing_subscriber::fmt()
         .with_target(false)
